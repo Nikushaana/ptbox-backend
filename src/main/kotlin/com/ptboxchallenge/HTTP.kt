924702253db1ef99425019b97d35adbb84451c81
Nikushaana <nikanashvili@gmail.com>
2025-01-15 17:01:04 +0400

    change

--- src/main/kotlin/com/ptboxchallenge/HTTP.kt
+++ src/main/kotlin/com/ptboxchallenge/HTTP.kt
@@ -5,13 +5,13 @@ import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
 
 fun Application.configureHTTP() {
-//    install(CORS) {
-//        allowMethod(HttpMethod.Get)
-//        allowMethod(HttpMethod.Post)
-//        allowMethod(HttpMethod.Put)
-//        allowMethod(HttpMethod.Delete)
-//        allowHeader(HttpHeaders.ContentType)
-//        allowCredentials = true
-//        allowHost("ptbox-frontend.vercel.app", schemes = listOf("https"))
-//    }
+    install(CORS) {
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowHeader(HttpHeaders.ContentType)
+        allowCredentials = true
+        allowHost("ptbox-frontend.vercel.app", schemes = listOf("https"))
+    }
 }
