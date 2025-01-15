7fbfc0607a7898a06b7338c037af67eb58c6eab3
Nikushaana <nikanashvili@gmail.com>
2025-01-15 16:23:00 +0400

    change

--- src/main/kotlin/com/ptboxchallenge/HTTP.kt
+++ src/main/kotlin/com/ptboxchallenge/HTTP.kt
@@ -13,6 +13,6 @@ fun Application.configureHTTP() {
         allowHeader(HttpHeaders.ContentType)
         allowHeader(HttpHeaders.Authorization)
         allowCredentials = true
-        allowHost("*")
+        allowHost("https://ptbox-frontend.vercel.app/", schemes = listOf("http", "https"))
     }
 }
